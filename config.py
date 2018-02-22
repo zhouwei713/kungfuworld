d070e7602888feee42ba0f7cd4e7bddda210896d
0027008515 <edisonvera@ZengYunYun.local>
2018-02-22 21:49:51 +0300

    add env data

--- config.py
+++ config.py
@@ -35,8 +35,8 @@ class DevelopmentConfig(Config):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     #SQLALCHEMY_DATABASE_URI = 'postgres://xqsibfufbyusjl:43d0a56c954439675f98a97dc354ced805a18e73548a7e8c71791a2b32f35d27@ec2-54-235-72-121.compute-1.amazonaws.com:5432/d5lp58cqt398vd'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/zhouluobo'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLURI')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/zhouluobo'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLURI')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class TestingConfig(Config):
